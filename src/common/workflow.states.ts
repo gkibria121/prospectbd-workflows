46114e9600a40ed75cccf0e183886e5821c9f8ca
Ahmed Kibria <gkibria121@gmail.com>
2026-05-07 21:44:55 +0600

    feat: export WorkflowStateId type derived from WorkflowStateMap keys

--- src/common/workflow.states.ts
+++ src/common/workflow.states.ts
@@ -1,3 +1,6 @@
 import { WORKFLOW_SYSTEM } from "./registry";
 
 export const WorkflowStateMap = WORKFLOW_SYSTEM.STATE_MAP;
+export type WorkflowStateId = {
+  [K in keyof typeof WorkflowStateMap]: keyof (typeof WorkflowStateMap)[K];
+}[keyof typeof WorkflowStateMap];
