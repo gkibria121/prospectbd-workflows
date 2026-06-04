51eeac177c0527003c76de1486c0d08d73f01930
Ahmed Kibria <gkibria121@gmail.com>
2026-06-04 14:18:33 +0600

    refactor(escalations): remove obsolete escalation strategies and related files

--- src/common/escalations/index.ts
+++ src/common/escalations/index.ts
@@ -1,7 +1,1 @@
-export * from "./interfaces/escalation.interface";
 export * from "./interfaces/alert.interface";
-export * from "./strategies/const.strategy";
-export * from "./strategies/standard.strategy";
-export * from "./strategies/urgent.strategy";
-export * from "./strategies/priority-next-day.strategy";
-export * from "./factory/escalation.factory";
